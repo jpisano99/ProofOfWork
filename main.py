f4aa0d5edfe5ad8883fa45c942e5678f1bdc8eb5
Jim Pisano <jpisano@cisco.com>
2018-01-15 12:31:07 -0500

    update

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ import hashlib
 #   Previous BlockHash
 #   Merkle Root : Hash of Hashes
 #   Timestamp
-#   Difficulty Target
-#   Nonce: What we are trying to guess
+#   Difficulty Target: Set by the network, the number of leading zeros to guess
+#   Nonce: What we are trying to guess it solves for the difficulty target
 # once solved miner hashes the header twice with SHA256 to become the new current block hash
 # https://www.youtube.com/watch?v=RPP2f_Fn8iQ
 
-my_string = 'Hello World!'
-my_string_hash = hashlib.sha256(my_string.encode('utf-8')).hexdigest()
+my_header = 'Hello World!'
+my_header_hash = hashlib.sha256(my_header.encode('utf-8')).hexdigest()
 # nonce = str(1)
-# pow = my_string + nonce
-# print (my_string,pow)
-# print ('String >>>  ', hashlib.sha256(my_string.encode('utf-8')).hexdigest())
+# pow = my_header + nonce
+# print (my_header,pow)
+# print ('String >>>  ', hashlib.sha256(my_header.encode('utf-8')).hexdigest())
 # print ('String + nonce >>> ',hashlib.sha256(pow.encode('utf-8')).hexdigest())
 
 num_matched = 0
@@ -24,7 +24,7 @@ num_matched = 0
 # One million guesses to match 5 leading zeroes
 for x in range (0,1000000):
     nonce = str(x)
-    pow = my_string + nonce
+    pow = my_header + nonce
     my_pow_hash = hashlib.sha256(pow.encode('utf-8')).hexdigest()
 
     if my_pow_hash[:5] == '00000' :
